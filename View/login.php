74d0777cbbe5816399b5d124c3ccb9ccb2dcc38c
seb-cc <csebas459@gmail.com>
2022-06-29 18:50:48 -0400

    UPDATE: Added log in page, made all pages responsive and started miragration to PHP

---
+++ View/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <title>Tracklys - Log in</title>
+        <link rel="stylesheet" href="..\Source\login.css">
+    </head>
+    <body>
+        <?php include_once '../By_default/header.html'; ?>
+        <div>
+            <input id="userId" placeholder="Name / Email Address">
+            <input id="userPassword" type="password" placeholder="Password">
+            <div class="button">
+                <input id="singup" type="button" value="Sign Up">
+                <input id="submit" type="button" value="Log In">
+            </div>
+        </div>
+    </body>
+</html>
